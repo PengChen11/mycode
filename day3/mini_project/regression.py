9e1e6e67e593a7b7addb7e2e21e92b97c95c2ffb
Peng Chen <peng.chen@tlgcohort.com>
2021-08-05 06:14:45 +0000

    updated

--- day3/mini_project/regression.py
+++ day3/mini_project/regression.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 import seaborn as sns
 
 """
@@ -25,17 +23,12 @@ age = np.asarray(list(range(18,65)))
 # becasue of groupby issue, I have to make a new data frame object
 new_df = pd.DataFrame({'age': age, 'charges':mean_charge_by_age})
 
+# setup seaborn to make a better looking plot and output it
 sns.set()
-plt.figure(figsize=(10,6),dpi=300)
+plt.figure(figsize=(10,6),dpi=100)
 sns.regplot( x="age", y='charges', data=new_df, order=1)
 plt.savefig("output.png")
 
 
-x=age.reshape(-1,1)
-y=mean_charge_by_age
-x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.8,test_size=0.2,random_state=100)
-
-lm = LinearRegression()
-lm.fit(x_train,y_train)
-print(f"Train accuracy {round(lm.score(x_train,y_train)*100,2)} %")
-print(f"Test accuracy {round(lm.score(x_test,y_test)*100,2)} %")
+print(f"\nPlease check the png file for plot. ")
+print(f"\n1. Based on the Linear regression model, we can see that the patients' average medical charges are related to the age.\n2. In general, the higher the patients' age, the higher the medical charges.")
