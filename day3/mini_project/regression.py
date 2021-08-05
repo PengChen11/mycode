b7ed3498a976bbbab1d66b23cb8c56148cb67d2b
Peng Chen <peng.chen@tlgcohort.com>
2021-08-05 06:05:34 +0000

    updated

--- day3/mini_project/regression.py
+++ day3/mini_project/regression.py
@@ -5,20 +5,31 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
+"""
+This program take an insruance costs data sheet with patients in different age group, trying to see whether the medical cost is related to age
+"""
+
+
 insurance_df = pd.read_csv('./insurance.csv')
 
-insurance_df.head(5)
+#  will choose only "age" and "charges" colums, group data by "age", calculate the mean for each age group,
+#  then round it to 0 digit, take the value of "charges" coloum (ages will no longer be used)
 
 mean_charge_by_age = insurance_df[["age","charges"]].groupby("age").mean().round(0)["charges"].values
 
+# groupby returns a dataFrameGroupBy instance, it is different to data frame. that's why I can only take the mean values.
 
+# we know the age start and ending point, now making an age array with numpy
 age = np.asarray(list(range(18,65)))
 
+# becasue of groupby issue, I have to make a new data frame object
 new_df = pd.DataFrame({'age': age, 'charges':mean_charge_by_age})
 
 sns.set()
 plt.figure(figsize=(10,6),dpi=300)
 sns.regplot( x="age", y='charges', data=new_df, order=1)
+plt.savefig("output.png")
+
 
 x=age.reshape(-1,1)
 y=mean_charge_by_age
