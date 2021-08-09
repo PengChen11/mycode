99631a764050351d1bca108749170950482c0e84
Peng Chen <peng.chen@tlgcohort.com>
2021-08-09 16:22:37 +0000

    all done

--- day6/challenge01.py
+++ day6/challenge01.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import datetime
+import reverse_geocoder as rg
 
 url = "http://api.open-notify.org/iss-now.json"
 
@@ -15,5 +16,6 @@ CURRENT LOCATION OF THE ISS:
 Timestamp: {datetime_time}
 Lon: {res_dict["iss_position"]["longitude"]}
 Lat: {res_dict["iss_position"]["latitude"]}
+City: {rg.search((res_dict["iss_position"]["longitude"], res_dict["iss_position"]["latitude"]))[0]["name"]}
 
 """)
